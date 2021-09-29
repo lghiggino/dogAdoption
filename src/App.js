e63c3b3dd36f8bade6538e953da7f6ca06a0a4e5
lghiggino <lghiggino@gmail.com>
2021-09-29 10:03:31 -0300

    navbar first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
 import logo from './logo.svg';
 import './App.css';
 import Tick from './components/Tick';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import About from './pages/About';
+import Home from './pages/Home';
+import Users from './pages/Users';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Tick />
-      </header>
-    </div>
-  );
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p>
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //       <Tick />
+    //     </header>
+    //   </div>
+    // );
+    return (
+        <Router>
+            <div>
+                <nav className="navbar">
+                    <ul className="navbar-ul">
+                        <li className="navbar-li-1">
+                            logo
+                        </li>
+                        <li className="navbar-li-5">
+                            <div className="navbar-center-div">
+                                <Link to="/">All Dogs</Link>
+                                <Link to="/services">Services</Link>
+                                <Link to="/about">About Us</Link>
+                                <Link to="/contact">Contact</Link>
+                            </div>
+                        </li>
+                        <li className="navbar-li-1">
+                            search
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                    <Route path="/services">
+                        <About />
+                    </Route>
+                    <Route path="/about">
+                        <Users />
+                    </Route>
+                    <Route path="/contact">
+                        <Home />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Users.js
@@ -0,0 +1,10 @@
+import Tick from "../components/Tick";
+
+export default function Users() {
+    return (
+        <>
+            <h1>This is the users page</h1>
+            <Tick />
+        </>
+    )
+}
